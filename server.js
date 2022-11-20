f50db87b8df58a6201d48fddea1e0ebfec6bb7f8
mertangy <ericmagesho@gmail.com>
2022-11-20 20:21:59 +0300

    updated

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the databases
-//let mongodb_url = 'mongodb://localhost:27017/';
-let mongodb_url = process.env.MONGODB_URI;
+let mongodb_url = 'mongodb+srv://mertangy:mertangy2022@gallery.cj6v9ym.mongodb.net/darkroom?retryWrites=true&w=majority';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
